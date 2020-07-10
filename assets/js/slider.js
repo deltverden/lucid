d8bbe396a05ba93894b122bb2a83c4247c7e5a8a
deltverden <d.bilets@edu.internetdevels.com>
2020-07-10 11:37:46 +0300

    Completed testimonials, advert-board, pricing sections. Completed comments section with slider

---
+++ assets/js/slider.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: false
+});
